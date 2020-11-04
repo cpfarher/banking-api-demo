db3f63d4d6c5c3669811e35f62c6504f40b27806
Christian <christianpfarher@argeniss.com>
2020-11-04 15:08:58 -0300

    change some env variables

--- app/config/config.js
+++ app/config/config.js
@@ -11,7 +11,8 @@ module.exports = {
   HOST: process.env.HOST || "127.0.0.1",
   PORT: process.env.PORT || 8080,
   DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
 };
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,9 +3,10 @@ const dbConfig = require("./config.js");
 
 module.exports = {
   HOST: `${dbConfig.DB_HOST}`, // localhost when is local,db_postgres for production
-  USER: `${dbConfig.DB_USER}`,
+  USER: `${dbConfig.DB_USERNAME}`,
   PASSWORD: `${dbConfig.DB_PASSWORD}`,
   DB: `${dbConfig.DB_NAME}`,
+  PORT: `${dbConfig.DB_PORT}`,
   dialect: "postgres",
   pool: {
     max: 5,
