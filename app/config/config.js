1c90f70e2328e12e0329e1ab3a23406e909d7ee7
Christian <christianpfarher@argeniss.com>
2020-11-04 08:49:59 -0300

    add env files

---
+++ app/config/config.js
@@ -0,0 +1,17 @@
+// config.js
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + ".env"),
+});
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "127.0.0.1",
+  PORT: process.env.PORT || 8080,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+};
