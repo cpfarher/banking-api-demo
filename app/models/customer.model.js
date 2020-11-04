b886d5851a80be89a7df7aecf84d0096ddb5a3a5
Christian <christianpfarher@argeniss.com>
2020-11-04 15:11:41 -0300

    add unqiue index

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -1,34 +1,43 @@
- /**
-   * @swagger
-   * definitions:
-   *   Customer:
-   *     type: object
-   *     required:
-   *       - name
-   *     properties:
-   *       id:
-   *         type: integer
-   *       name:
-   *         type: string
-   *         description: Name of the customer
-   *         required: true
-   *         unique: true
-   *       passport:
-   *         type: string
-   *         required: false
-   *         unique: true
-   *         description: optional Passport of the customer
-   */
+/**
+  * @swagger
+  * definitions:
+  *   Customer:
+  *     type: object
+  *     required:
+  *       - name
+  *     properties:
+  *       id:
+  *         type: integer
+  *       name:
+  *         type: string
+  *         description: Name of the customer
+  *         required: true
+  *         unique: true
+  *       passport:
+  *         type: string
+  *         required: false
+  *         unique: true
+  *         description: optional Passport of the customer
+  */
 
 module.exports = (sequelize, Sequelize) => {
-  const Customer = sequelize.define("customer", {
-    name: {
-      type: Sequelize.STRING,
+  const Customer = sequelize.define(
+    "customer",
+    {
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: true,
+        },
+      },
+      passport: {
+        type: Sequelize.STRING,
+      },
     },
-    passport: {
-      type: Sequelize.STRING,
-    },
-  });
+    { indexes: [{ unique: true, fields: ["name"] }] }
+  );
 
   return Customer;
 };
